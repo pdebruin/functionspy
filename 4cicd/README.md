4e370c3a8d797749e89a6f2dc20fb3c782cda762
Pieter de Bruin <pdebruin@hotmail.com>
2024-04-05 15:53:32 +0200

    2 functions in 1 file?

--- 4cicd/README.md
+++ 4cicd/README.md
@@ -5,11 +5,21 @@ This sample doesn't follow a specific previous step, although code of [an existi
 
 You can configure CICD in a few ways including Azure Functions deployment center, az cli, and manually in GitHub. 
 
+Deployment center gave me the github action yml in this folder. The changes I made:
+* Update the name
+
+* Update the AZURE_FUNCTIONAPP_PACKAGE_PATH to the folder with the function to be built
+
+* Update the trigger (on | push | path) to only respond to changes in the function's folder 
+
 ```
 
 
 
 ```
+At this point you can use the so called inner loop to develop and run functions locally and use the outerloop to store edits in version control with automated deployment to a central location for others to access them. 
+
+When your functions are ready for production you should look at Staging slots, which help the experience of your Functions' users.
 
 [Source](https://learn.microsoft.com/azure/azure-functions/functions-how-to-github-actions)
 
---
+++ 4cicd/function_app.py
@@ -0,0 +1,35 @@
+import azure.functions as func
+import datetime
+import json
+import logging
+
+app = func.FunctionApp()
+
+@app.route(route="HttpExample", auth_level=func.AuthLevel.ANONYMOUS)
+@app.queue_output(arg_name="msg", queue_name="outqueue", connection="AzureWebJobsStorage")
+def HttpExample(req: func.HttpRequest, msg: func.Out [func.QueueMessage]) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        msg.set(name)
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed CICD successfully.")
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed CICD successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
+
+@app.queue_trigger(arg_name="azqueue", queue_name="outqueue",
+                               connection="AzureWebJobsStorage") 
+def QueueExample(azqueue: func.QueueMessage):
+    logging.info('Python Queue trigger processed a CICD message: %s',
+                azqueue.get_body().decode('utf-8'))
